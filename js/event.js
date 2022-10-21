34a972c2a4ef99ad02da3febabf2a998f7c3292d
winvdaking <pro.dorianlopez@gmail.com>
2022-10-21 11:11:25 +0200

    feat(#level_03): add score method

---
+++ js/event.js
@@ -0,0 +1,38 @@
+import { lower, greater, win } from 'status.js';
+import { displayScores, displayScoresByUser, saveScore } from './score.js';
+
+const input = document.getElementById('inputNumber');
+const inputName = document.getElementById('inputName');
+const spanResult = document.getElementById('result');
+
+export function run(number) {
+
+    let score = 0;
+
+    console.log(number);
+
+    input.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+            score++;
+            if (input.value > number) {
+                lower(score);
+            } else if (input.value < number) {
+                greater(score);
+            } else {
+                win();
+            }
+        }
+    });
+
+    inputName.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter') {
+            inputName.setAttribute('disabled', '');
+            saveScore(inputName.value.trim(), score);
+            spanResult.innerText = displayScoresByUser(inputName.value.trim());
+        }
+    });
+}
+
+export function score(){
+    
+}
--- js/ui.js
+++ js/ui.js
@@ -1,45 +1,23 @@
 import { random } from './random.js';
-import { greater, lower, win } from './status.js';
-import { displayScores, displayScoresByUser, saveScore } from './score.js';
+import { run, score } from './event.js';
 
 export function init() {
     eventKey(random());
 }
 
-const input = document.getElementById('inputNumber');
-const inputName = document.getElementById('inputName');
-const spanResult = document.getElementById('result');
 const btnRun = document.getElementById('run');
 const btnScore = document.getElementById('score');
 const btnReset = document.getElementById('reset');
 const btnScoreBy = document.getElementById('score-by');
 
-let score = 0;
-
 function eventKey(number) {
 
     btnRun.addEventListener('click', (e) => {
-        console.log(number);
-        input.addEventListener('keyup', (e) => {
-            if (e.key === 'Enter') {
-                score++;
-                if (input.value > number) {
-                    lower(score);
-                } else if (input.value < number) {
-                    greater(score);
-                } else {
-                    win();
-                }
-            }
-        });
+        run(number);
+    });
 
-        inputName.addEventListener('keyup', (e) => {
-            if (e.key === 'Enter') {
-                inputName.setAttribute('disabled', '');
-                saveScore(inputName.value.trim(), score);
-                spanResult.innerText = displayScoresByUser(inputName.value.trim());
-            }
-        });
-    })
+    btnScore.addEventListener('click', (e) => {
+        score();
+    });
 }
 
