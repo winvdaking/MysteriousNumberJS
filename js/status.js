e10c462f5d7425170de744d41f3e314bb6f2b6c5
winvdaking <pro.dorianlopez@gmail.com>
2022-10-21 10:58:13 +0200

    fix

---
+++ js/status.js
@@ -0,0 +1,22 @@
+const spanResult = document.getElementById('result');
+const input = document.getElementById('inputNumber');
+const inputName = document.getElementById('inputName');
+
+export function lower(score) {
+    spanResult.innerText = `Le nombre mystérieux est plus petit ! | Nombre d'essai${score > 1 ? 's' : ''}: ${score}`;
+    input.select();
+}
+
+export function greater(score) {
+    spanResult.innerText = `Le nombre mystérieux est plus grand ! | Nombre d'essai${score > 1 ? 's' : ''}: ${score}`;
+    input.select();
+}
+
+export function win() {
+    spanResult.innerText = "Félicitations, vous avez gagné !";
+    input.setAttribute('disabled', '');
+    input.classList.add('success');
+    inputName.classList.remove('name');
+    inputName.focus();
+    document.getElementById('lblNumber').classList.add('success');
+}
