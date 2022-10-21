49fb3aa11044095da5070cd96ebf4b644a8e9125
winvdaking <pro.dorianlopez@gmail.com>
2022-10-21 11:03:46 +0200

    feat(#level_03): add ui btn

--- js/score.js
+++ js/score.js
@@ -13,8 +13,12 @@ export function saveScore(username, score = 0){
 	return player || null;
 }
 
-export function displayScores(username){
-    let scoreboard = scores.sort((a,b) => a.score - b.score);
+export function displayScoresByUser(username){
+    let scoreboard = displayScores();
     let position = scoreboard.findIndex(u => u.username === username)+1;
     return `Vous êtes ${position}${position == 1 ? 'er' : 'ème'} !`;
+}
+
+export function displayScores(){
+    return scores.sort((a,b) => a.score - b.score);
 }
--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,6 @@
 import { random } from './random.js';
 import { greater, lower, win } from './status.js';
-import { displayScores, saveScore } from './score.js';
+import { displayScores, displayScoresByUser, saveScore } from './score.js';
 
 export function init() {
     eventKey(random());
@@ -9,29 +9,37 @@ export function init() {
 const input = document.getElementById('inputNumber');
 const inputName = document.getElementById('inputName');
 const spanResult = document.getElementById('result');
+const btnRun = document.getElementById('run');
+const btnScore = document.getElementById('score');
+const btnReset = document.getElementById('reset');
+const btnScoreBy = document.getElementById('score-by');
 
 let score = 0;
 
 function eventKey(number) {
-    console.log(number);
-    input.addEventListener('keyup', (e) => {
-        if (e.key === 'Enter') {
-            score++;
-            if (input.value > number) {
-                lower(score);
-            } else if (input.value < number) {
-                greater(score);
-            } else {
-                win();
+
+    btnRun.addEventListener('click', (e) => {
+        console.log(number);
+        input.addEventListener('keyup', (e) => {
+            if (e.key === 'Enter') {
+                score++;
+                if (input.value > number) {
+                    lower(score);
+                } else if (input.value < number) {
+                    greater(score);
+                } else {
+                    win();
+                }
             }
-        }
-    });
-    
-    inputName.addEventListener('keyup', (e) => {
-        if (e.key === 'Enter') {
-            inputName.setAttribute('disabled', '');
-            saveScore(inputName.value.trim(), score);
-            spanResult.innerText = displayScores(inputName.value.trim());
-        }
-    });
+        });
+
+        inputName.addEventListener('keyup', (e) => {
+            if (e.key === 'Enter') {
+                inputName.setAttribute('disabled', '');
+                saveScore(inputName.value.trim(), score);
+                spanResult.innerText = displayScoresByUser(inputName.value.trim());
+            }
+        });
+    })
 }
+
