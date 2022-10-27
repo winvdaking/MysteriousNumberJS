b91c3116b31aa5cebc7b2ea30a03ed2a0997b17d
winvdaking <pro.dorianlopez@gmail.com>
2022-10-27 12:59:28 +0200

    feat(ui): add new node DOM

--- js/ui.js
+++ js/ui.js
@@ -2,31 +2,35 @@ import { random } from './random.js';
 import { run, score } from './event.js';
 import { displayScoresByUser } from './score.js';
 
-const btnRun = document.getElementById('run');
-const btnScore = document.getElementById('score');
-const btnReset = document.getElementById('reset');
-const btnScoreBy = document.getElementById('score-by');
+const $btnRun = document.getElementById('run');
+const $btnScore = document.getElementById('scores');
+const $btnReset = document.getElementById('reset');
+const $btnScoreBy = document.getElementById('score-by');
+const $table = document.getElementById('table');
+const $run = document.getElementById('runDiv');
 
 export function init() {
 
-    btnRun.addEventListener('click', (e) => {
+    $btnRun.addEventListener('click', (e) => {
+        $table.classList.add('none');
+        $run.classList.remove('none');
         run(random());
     });
 
-    btnScore.addEventListener('click', (e) => {
+    $btnScore.addEventListener('click', (e) => {
         score();
     });
 
-    btnScoreBy.addEventListener('click', (e) => {
-        let inputSearch = document.getElementById('inputSearch');
-        inputSearch.addEventListener('keyup', (e) => {
+    $btnScoreBy.addEventListener('click', (e) => {
+        let $inputSearch = document.getElementById('inputSearch');
+        $inputSearch.addEventListener('keyup', (e) => {
             if (e.key === 'Enter') {
                 displayScoresByUser(inputSearch.value.trim());
             }
         })
     });
 
-    btnReset.addEventListener('click', (e) => {
+    $btnReset.addEventListener('click', (e) => {
         run(random());
     });
 }
