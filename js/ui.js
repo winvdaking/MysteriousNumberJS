77c55c14727faa49fb4118aa3c17f01c02e11e0b
winvdaking <pro.dorianlopez@gmail.com>
2022-10-27 13:13:12 +0200

    feat(ui): add loadScore()

--- js/ui.js
+++ js/ui.js
@@ -8,8 +8,11 @@ const $btnReset = document.getElementById('reset');
 const $btnScoreBy = document.getElementById('score-by');
 const $table = document.getElementById('table');
 const $run = document.getElementById('runDiv');
+const $tbody = document.getElementById('tbody');
 
 export function init() {
+
+    loadScore();
 
     $btnRun.addEventListener('click', (e) => {
         $table.classList.add('none');
@@ -18,7 +21,7 @@ export function init() {
     });
 
     $btnScore.addEventListener('click', (e) => {
-        score();
+        loadScore();
     });
 
     $btnScoreBy.addEventListener('click', (e) => {
@@ -32,5 +35,17 @@ export function init() {
 
     $btnReset.addEventListener('click', (e) => {
         run(random());
+    });
+}
+
+function loadScore(){
+    score().forEach(usr => {
+        let $tr = document.createElement('tr');
+        let $td1 = document.createElement('td');
+        let $td2 = document.createElement('td');
+        $td1.innerText = usr.username;
+        $td2.innerText = usr.score;
+        $tr.append($td1, $td2);
+        $tbody.appendChild($tr);
     });
 }
