bef460c2fdc80a22c4fc1c49ac55c6d4ab1969b0
winvdaking <pro.dorianlopez@gmail.com>
2022-10-28 09:40:58 +0200

    chore(ui/config): segregation config nodeElement

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,7 @@
 import { random } from './random.js';
 import { run, score, resetScore } from './event.js';
 import { displayScoreByUser } from './score.js';
-
-const $btnRun = document.getElementById('run');
-const $btnScore = document.getElementById('scores');
-const $btnReset = document.getElementById('reset');
-const $btnScoreBy = document.getElementById('score-by');
-const $table = document.getElementById('table');
-const $run = document.getElementById('runDiv');
-const $tbody = document.getElementById('tbody');
-const $inputSearch = document.getElementById('inputSearch');
-const $divScore = document.getElementById('scoreBy');
-const $divRunBtn = document.getElementById('runDivBtn');
-const $btnsRun = document.getElementById('btnsRun');
-const $btnEasy = document.getElementById('easy');
-const $btnMedium = document.getElementById('medium');
-const $btnHard = document.getElementById('hard');
-const $btnImpo = document.getElementById('impossible');
-const $lblNumber = document.getElementById('lblNumber');
+import { $btnRun, $btnScore, $btnReset, $btnScoreBy, $table, $run, $tbody, $inputSearch, $divScore, $divRunBtn, $btnsRun, $btnEasy, $btnMedium, $btnHard, $btnImpo, $lblNumber } from './config.js';
 
 export function init() {
 
